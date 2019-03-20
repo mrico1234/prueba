7b4665c2ce3b37545dfed0f3c941341810607a51
Maikol Rico perez <mrico@unitecnica.net>
2019-03-19 20:06:39 -0500

    Se implento q muestre todas las ordenes en la vista.

--- app/assets/javascripts/requisitions.js
+++ app/assets/javascripts/requisitions.js
@@ -1,14 +1,26 @@
 function traer(){
+	
+	let url = `http://localhost:3000/ordens`
 	var request = new XMLHttpRequest()
 
-	request.open('GET', 'http://localhost:3000/ordens', true)
+	request.open('GET', url, true)
 	request.onload = function() {
 	  // Begin accessing JSON data here
 	  var data = JSON.parse(this.response)
+	  //let resultado = document.getElementById("resultado")
+	  //resultado.innerHtml = '';
 
 	  if (request.status >= 200 && request.status < 400) {
-	    data.forEach(movie => {
-	      console.log(movie.id+"--"+movie.code+"--"+movie.branch)
+	    data.forEach(order => {
+	      //console.log(orden.id+"--"+orden.code+"--"+orden.branch)
+	      //console.log(data)
+	      //alert(`Pedido numero: ${order.id}\nCódigo: ${order.code} status: ${order.status}`);
+	      console.log(data);	
+	      document.getElementById("parrafo").innerHTML += `<li>Pedido numero: ${order.id} Codigo: ${order.code} Estatus: ${order.status} Cliente: ${order.Cliente}</li>`;
+	      
+
+	      
+	 
 	    })
 	  } else {
 	    console.log('error')
